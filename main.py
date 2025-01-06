af039a3985d2472aab3b0268acf0037ba4e59739
Vk602504 <vikashkumarjalpura4@gmail.com>
2025-01-06 08:17:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from config import Config
 from pyrogram import Client as VJ, idle
 from typing import Union, Optional, AsyncGenerator
 from logging.handlers import RotatingFileHandler
-from plugins.regix import restart_forwards
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
